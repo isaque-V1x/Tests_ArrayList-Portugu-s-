820a83e6c901fb6e3c9ab677bc05e44ee83d48b1
Isaque Alves <isaquealves2017@gmail.com>
2025-08-15 00:01:34 -0300

    Código em português

--- src/Tests_ArrayLists/Main.java
+++ src/Tests_ArrayLists/Main.java
@@ -1,10 +1,46 @@
 package Tests_ArrayLists;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	    ArrayList<Person> listOfPerson = new ArrayList<>();
+	    Scanner sc = new Scanner(System.in);
 
+	    System.out.println("Olá, bem-vindo à minha primeira aplicação prática em Java: trabalhando com listas e coleções de dados.");
+	    System.out.println("------------------------------------------------------------------------------------------------------");
+	    System.out.println();
+
+	    System.out.print("Quantas pessoas você deseja adicionar à lista: ");
+	    int quantities = sc.nextInt();
+	    System.out.println();
+	    sc.nextLine();
+
+	    for (int i = 0; i < quantities; i++) {
+	        Person p = new Person();
+	        System.out.print("Nome: ");
+	        p.setNome(sc.nextLine());
+	        System.out.print("Idade: ");
+	        p.setIdade(sc.nextInt());
+	        System.out.println("-------------");
+	        sc.nextLine();
+
+	        listOfPerson.add(p);
+	    }
+
+	    System.out.println("O tamanho da lista é: " + listOfPerson.size());
+
+	    if (!listOfPerson.isEmpty()) {
+	        System.out.println("A primeira pessoa da lista é: " + listOfPerson.get(0));
+	    }
+
+	    for (Person person : listOfPerson) {
+	        System.out.println(person);
+	    }
+
+	    sc.close();
 	}
 
 }
--- src/Tests_ArrayLists/Person.java
+++ src/Tests_ArrayLists/Person.java
@@ -1,5 +1,27 @@
 package Tests_ArrayLists;
 
+
 public class Person {
 
+    private String nome;
+    private Integer idade;
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public Integer getIdade() {
+        return idade;
+    }
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    @Override
+    public String toString() {
+        return "Nome da pessoa: " + nome + ", Idade: " + idade;
+    }
 }
+
